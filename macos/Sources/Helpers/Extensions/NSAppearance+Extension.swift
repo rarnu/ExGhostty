825ddbcc30a79d1d18a904c7d4c22383627f689c
rarnu <rarnu1985@gmail.com>
2026-07-20 17:12:30 +0800

    add: UI锁定为深色

--- macos/Sources/Helpers/Extensions/NSAppearance+Extension.swift
+++ macos/Sources/Helpers/Extensions/NSAppearance+Extension.swift
@@ -7,25 +7,8 @@ extension NSAppearance {
     }
 
     /// Initialize a desired NSAppearance for the Ghostty configuration.
+    /// 当前固定为深色模式，用户无法在配置中修改。
     convenience init?(ghosttyConfig config: Ghostty.Config) {
-        guard let theme = config.windowTheme else { return nil }
-        switch theme {
-        case "dark":
-            self.init(named: .darkAqua)
-
-        case "light":
-            self.init(named: .aqua)
-
-        case "auto":
-            let color = OSColor(config.backgroundColor)
-            if color.isLightColor {
-                self.init(named: .aqua)
-            } else {
-                self.init(named: .darkAqua)
-            }
-
-        default:
-            return nil
-        }
+        self.init(named: .darkAqua)
     }
 }
